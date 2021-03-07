b00bf3adb96390f631fd04741923fe9386744f47
JorgePadilla <jorgep4dill4@gmail.com>
2021-03-07 03:03:43 -0600

    Create Homepage for Authorized Users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+  authenticated :user do
+    root "pages#invoices", as: :authenticated_root
+  end
   root 'pages#home'
-  get 'pages/invoices'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
